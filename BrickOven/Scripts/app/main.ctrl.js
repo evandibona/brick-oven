111f919a9dedc6b1c783cd70eec9c39c67bd8adf
Evan DiBona <evandibona@gmx.com>
2015-03-03 17:19:17 -0500

    Added a function for handling selected form elements.

--- BrickOven/Scripts/app/main.ctrl.js
+++ BrickOven/Scripts/app/main.ctrl.js
@@ -6,9 +6,14 @@
 
     function carController($http, BrickFactory) {
         var vm = this
-        vm.info = { Car: "Could go here" } 
+        vm.info = { Car: "Could go here" }
         vm.years = ["Years couldn't load"]
         vm.makes = ["Makes couldn't load"]
+        vm.headsel = [false, false, false, false, false]
+        vm.cars = [
+            { make: "Shiny", model: "Rocket", seats: "18", year: "1950", body: "Rocket Shaped", fuel: "Lots of It." },
+            { make: "Buggy", model: "Smithfield Co.", seats: "2", year: "1750", body: "Horse shoe", fuel: "Horses." },
+        ]
         /*
         $http.post("/api/finder/years", { n: "nothing" })
             .success(function (data, status, headers, config) {
@@ -25,5 +30,15 @@
                 Console.log("Error!!", data)
             })
             */
+        vm.select = function (i) {
+            function fillAry(a, f) {
+                for (var i in a) {
+                    a[i] = f
+                }
+            }
+
+            fillAry(vm.headsel, false)
+            vm.headsel[i] = true
+        }
     }
 })()
