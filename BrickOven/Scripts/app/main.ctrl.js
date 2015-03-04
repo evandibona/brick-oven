3e398b4c482db7b88a39adbccbd3c4c27f6d1259
Evan DiBona <evandibona@gmx.com>
2015-03-04 11:17:04 -0500

    Successfully runs a function on submit. (Andrew helped)

--- BrickOven/Scripts/app/main.ctrl.js
+++ BrickOven/Scripts/app/main.ctrl.js
@@ -1,5 +1,12 @@
 ﻿(function () {
     'use strict'
+
+    function ForEach(ary, action) {
+        for (var i in ary) {
+            action(ary[i])
+        }
+    }
+
     angular
         .module('BrickOven')
         .controller('CarController', carController);
@@ -10,41 +17,35 @@
         vm.years = ["Years couldn't load"]
         vm.makes = ["Makes couldn't load"]
         vm.traits = [
-            ["make", false, "Make",     ""], 
-            ["model", false, "Model",   ""], 
-            ["seats", false, "Seats",   ""], 
-            ["year", false, "Year",     ""], 
-            ["body", false, "Body Type",""], 
-            ["fuel", false, "Fuel",     ""], 
+            ["make", false, "Make", ""],
+            ["model", false, "Model", ""],
+            ["seats", false, "Seats", ""],
+            ["year", false, "Year", ""],
+            ["body", false, "Body Type", ""],
+            ["fuel", false, "Fuel", ""],
         ]
         vm.cars = [
             { make: "Shiny", model: "Rocket", seats: "18", year: "1950", body: "Rocket Shaped", fuel: "Lots of It." },
             { make: "Buggy", model: "Smithfield Co.", seats: "2", year: "1750", body: "Horse shoe", fuel: "Horses." },
         ]
-        /*
-        $http.post("/api/finder/years", { n: "nothing" })
-            .success(function (data, status, headers, config) {
-                vm.years = data
-            })
-            .error(function (data, status, headers, config) {
-                Console.log("Error!!", data)
-            })
-        $http.post("/api/finder/makes", { n: "nothing" })
-            .success(function (data, status, headers, config) {
-                vm.makes = data
-            })
-            .error(function (data, status, headers, config) {
-                Console.log("Error!!", data)
-            })
-            */
-        vm.select = function (i) {
-            function fillTraits(a, f) {
-                for (var i in a) {
-                    a[i][1] = f
+        vm.filter = []
+        vm.makeFilter = function (traits) {
+            var filter = []
+            ForEach(traits, function (e) {
+                if (e[3].length > 0) {
+                    var a = e[0]
+                    filter += { a: e[3] }
                 }
+            })
+            return filter
+        }
+        vm.clearTraits = function() {
+            for (var i in vm.traits) {
+                vm.traits[i][1] = false
             }
-
-            fillTraits(vm.traits, false)
+        }
+        vm.select = function (i) {
+            vm.clearTraits()
             vm.traits[i][1] = true
         }
     }
--- BrickOven/Scripts/app/main.factory.js
+++ BrickOven/Scripts/app/main.factory.js
@@ -10,5 +10,21 @@
             what: "Is going on?" 
         }
         return msg
+        /*
+        $http.post("/api/finder/years", { n: "nothing" })
+            .success(function (data, status, headers, config) {
+                vm.years = data
+            })
+            .error(function (data, status, headers, config) {
+                Console.log("Error!!", data)
+            })
+        $http.post("/api/finder/makes", { n: "nothing" })
+            .success(function (data, status, headers, config) {
+                vm.makes = data
+            })
+            .error(function (data, status, headers, config) {
+                Console.log("Error!!", data)
+            })
+            */
     }
 })()
