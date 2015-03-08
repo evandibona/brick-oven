007f165e6ebdfcd14a8864bb4c7f3e4872db01d4
Evan DiBona <evandibona@gmx.com>
2015-03-08 12:15:33 -0400

    All property searches are minimally functional.

--- BrickOven/Scripts/app/main.ctrl.js
+++ BrickOven/Scripts/app/main.ctrl.js
@@ -7,20 +7,16 @@
     function carController($http, carApi) {
         var vm = this
         vm.traits = [
-            { sql: "make", name: "Make" },
-            { sql: "model_name", name: "Model" },
-            { sql: "seats", name: "Seats" },
-            { sql: "model_year", name: "Year" },
-            { sql: "body_style", name: "Body Type" },
-            { sql: "engine_fuel", name: "Fuel" },
+            { id: "make", name: "Make" },
+            { id: "modelName", name: "Model" },
+            { id: "seats", name: "Seats" },
+            { id: "modelYear", name: "Year" },
+            { id: "bodyStyle", name: "Body Type" },
+            { id: "engineFuel", name: "Fuel" },
         ]
         angular.forEach(vm.traits, function (e) {
-            carApi.list(e.sql, vm)
+            carApi.list(e.id, vm)
         })
-        console.log(vm.make)
-        console.log(vm.model_name)
-        console.log(vm.make)
-        console.log(vm.make)
         /*
         vm.filter = {}
         vm.makeFilter = function (traits) {
@@ -34,35 +30,6 @@
             vm.filter = vm.makeFilter(vm.traits)
         }
         */
-        function camel(action, str) {
-            var a = action.toLowerCase()
-            if (a == 'squish') {
-                var d = str.split('_')
-                var o = ""
-                // factor into a foreach
-                for (var i in d) {
-                    var e = d[i].toLowerCase()
-                    if (i != 0) {
-                        o += capitalize(e)
-                    }
-                    if (i == 0) {
-                        o += e
-                    }
-                }
-                return o
-            }
-            if (a == 'stretch') {
-                var ary = str.split(/(?=[A-Z])/)
-                for (var i in ary) {
-                    ary[i] = ary[i].toLowerCase()
-                }
-                return ary.join("_")
-            }
-            return "Incorrect action was used."
-            function capitalize(str) {
-                return str[0].toUpperCase() + str.slice(1)
-            }
-        }
 
 
     }
--- BrickOven/Scripts/app/main.factory.js
+++ BrickOven/Scripts/app/main.factory.js
@@ -15,11 +15,43 @@
         ///////////////
 
         function listProperty(prp, vm) {
-            $http.get("/api/finder/" + prp)
+            var sql = camel('stretch', prp )
+            $http.get("/api/finder/" + sql)
             .then(function (d) {
                 vm[prp] = d.data 
-                console.log(vm[prp])
             })
+        }
+
+        /////////
+        function camel(action, str) {
+            var a = action.toLowerCase()
+            if (a == 'squish') {
+                var d = str.split('_')
+                var o = ""
+                // factor into a foreach
+                for (var i in d) {
+                    var e = d[i].toLowerCase()
+                    if (i != 0) {
+                        o += capitalize(e)
+                    }
+                    if (i == 0) {
+                        o += e
+                    }
+                }
+                return o
+            }
+            if (a == 'stretch') {
+                var ary = str.split(/(?=[A-Z])/)
+                for (var i in ary) {
+                    ary[i] = ary[i].toLowerCase()
+                }
+                return ary.join("_")
+            }
+            return "Incorrect action was used."
+            //////////////////////
+            function capitalize(str) {
+                return str[0].toUpperCase() + str.slice(1)
+            }
         }
     }
 })()
